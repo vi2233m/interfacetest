ac071111ca7c502154b3ea01a22045a4a43ee224
vi2233m <31640283+vi2233m@users.noreply.github.com>
2019-03-21 23:50:10 +0800

    20190321

--- src/test/java/contact/DepartmentTest.java
+++ src/test/java/contact/DepartmentTest.java
@@ -3,9 +3,12 @@ package contact;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.assertj.core.api.Assertions.*;
 
 class DepartmentTest {
 
@@ -19,8 +22,14 @@ class DepartmentTest {
 
     @Test
     void list() {
-        department.list("").then().statusCode(200).body("department.name[0]", equalTo("定向班第一期"));
-//        department.list("33").then().statusCode(200).body("department.name[0]", equalTo("定向班第一期"));
+        department.list("").then().statusCode(200).log().all().body("department.name[0]", equalTo("零壹testing"));
+        department.list("2").then().statusCode(200).body("errmsg", equalTo("ok"));
+        String name = department.list("2").then().extract().body().jsonPath().get("department.name[0]");
+        List<String> names = department.list("").then().extract().body().jsonPath().get("department.name");
+
+        assertThat(name).isEqualTo("测试部");
+        assertThat(names).contains("测试部", "testvi22233").hasSize(5).doesNotContain("test");
+        assertThat(name).as("my department is %s", name).isEqualTo("测试");
     }
 
     @Test
